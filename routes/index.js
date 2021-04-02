1a96b962c5696065f5f5991d06f559100a6cdaa0
Cao Mai <caomai@rocketmail.com>
2021-04-01 22:38:37 -0700

    Implement search pagination

--- routes/index.js
+++ routes/index.js
@@ -20,16 +20,34 @@ app.get('/', (req, res) => {
   });
 });
 
-// SEARCH PET
-app.get('/search', (req, res) => {
-  term = new RegExp(req.query.term, 'i')
+// SEARCH PET WITHOUT PAGINATION
+// app.get('/search', (req, res) => {
+//   term = new RegExp(req.query.term, 'i')
 
-  Pet.find({$or:[
-    {'name': term},
-    {'species': term}
-  ]}).exec((err, pets) => {
-    res.render('pets-index', { pets: pets });
-  })
+//   Pet.find({$or:[
+//     {'name': term},
+//     {'species': term}
+//   ]}).exec((err, pets) => {
+//     res.render('pets-index', { pets: pets });
+//   })
+// });
+
+// SEARCH PET WITH PAGINATION
+app.get('/search', (req, res) => {
+
+  const term = new RegExp(req.query.term, 'i')
+
+  const page = req.query.page || 1
+  Pet.paginate(
+    {
+      $or: [
+        { 'name': term },
+        { 'species': term }
+      ]
+    },
+    { page: page }).then((results) => {
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term }); // need to add term here so pagination works with searching
+    });
 });
 
 }
