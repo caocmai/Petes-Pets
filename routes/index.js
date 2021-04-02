02ef56d72a7d839597ad2eb8102aedfcc94f7be0
Cao Mai <caomai@rocketmail.com>
2021-04-01 22:28:14 -0700

    Implement pagination on the home page

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,23 @@ const Pet = require('../models/pet');
 
 module.exports = (app) => {
 
-  /* GET home page. */
-  app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
-    });
+/* GET home page. just on first page*/ 
+// app.get('/', (req, res) => {
+//   Pet.paginate().then((results) => {
+//     res.render('pets-index', { pets: results.docs });    
+//   });
+// });
+
+/* GET home page. */
+app.get('/', (req, res) => {
+  const page = req.query.page || 1
+
+  Pet.paginate({}, {page: page}).then((results) => {
+    // res.render('pets-index', { pets: results.docs });    // works manually going to each url ie http://localhost:3000/?page=2 
+    res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page }); // moves to each page automatically
+
   });
+});
 
 // SEARCH PET
 app.get('/search', (req, res) => {
